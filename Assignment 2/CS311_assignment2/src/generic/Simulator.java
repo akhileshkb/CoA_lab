da0ffdb2c2b7213d079b19aacb835115fa67d3b7
cyberlust <rutikdadhaniya2034@gmail.com>
2020-09-23 18:20:21 +0530

    First 2 steps Done

--- Assignment 2/CS311_assignment2/src/generic/Simulator.java
+++ Assignment 2/CS311_assignment2/src/generic/Simulator.java
@@ -1,10 +1,14 @@
 package generic;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import generic.Operand.OperandType;
+import java.io.ObjectOutputStream;
+import java.io.File;
+import java.io.IOException;
 
 
-public class Simulator {
+public class Simulator{
 		
 	static FileInputStream inputcodeStream = null;
 	
@@ -23,6 +27,29 @@ public class Simulator {
 		//3. write the data to the file
 		//4. assemble one instruction at a time, and write to the file
 		//5. close the file
+		try 
+		{
+    		FileOutputStream fos = new FileOutputStream(new File(objectProgramFile));
+			int firstCodeAddress = ParsedProgram.firstCodeAddress;
+			ObjectOutputStream out = new ObjectOutputStream(fos);
+			out.write(firstCodeAddress);
+			// for (int i = 0; i < ParsedProgram.data.size(); i++)
+			// {
+			// 	out.write(ParsedProgram.data.get(i));
+			// }
+			// out.writeObject(ParsedProgram.data);
+			// out.writeObject(ParsedProgram.code);
+			// Serializer.serialize(ParsedProgram.code);
+			// for (int i = 0; i < ParsedProgram.code.size(); i++)
+			// {
+			// 	out.writeObject(ParsedProgram.code.get(i));
+			// } 
+			// fos.close();
+		} 
+		catch (IOException ex)
+		{
+	    	ex.printStackTrace();
+		}
 	}
 	
 }
