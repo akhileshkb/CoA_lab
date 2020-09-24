6a3e7bb820b3a82d16134d9c327acdfffd891619
cyberlust <rutikdadhaniya2034@gmail.com>
2020-09-24 15:30:12 +0530

    R3,RI Type Done. Please use same variables

--- Assignment 2/CS311_assignment2/src/generic/Simulator.java
+++ Assignment 2/CS311_assignment2/src/generic/Simulator.java
@@ -46,7 +46,6 @@ public class Simulator{
 			{
 				result = "";
 				int pc = ParsedProgram.code.get(i).getProgramCounter();
-				// dout.writeInt(pc);
 				Instruction.OperationType operation_type = ParsedProgram.code.get(i).operationType;
 				int opcode = operation_type.ordinal();
 				
@@ -54,7 +53,6 @@ public class Simulator{
 				
 				System.out.println(res);
 				
-
 				Operand sop1 = new Operand();
 				Operand sop2 = new Operand();
 				Operand dop = new Operand();
@@ -63,24 +61,40 @@ public class Simulator{
 				sop2= ParsedProgram.code.get(i).getSourceOperand2();
 				dop= ParsedProgram.code.get(i).getDestinationOperand();
 
-				if(opcode != 29 && opcode != 24)
+				if(sop2!=NULL) 				//Not RI type
 				{
-					int optype1 = sop1.getOperandType().ordinal();
-					int optype2 = sop2.getOperandType().ordinal();
-					int optype3 = dop.getOperandType().ordinal();	
+					if(opcode != 29 && opcode != 24)
+					{
+						int optype1 = sop1.getOperandType().ordinal();
+						int optype2 = sop2.getOperandType().ordinal();
+						int optype3 = dop.getOperandType().ordinal();	
+						
+						if(optype1 == 0 && optype2 == 0 && optype3 == 0)		//R3 type Operation
+						{
+							int rs1 = sop1.getValue();
+							int rs2 = sop2.getValue();
+							int rd = dop.getValue();
+							result = result + String.format("%5s", Integer.toBinaryString(rs1));
+
+							result = result + String.format("%5s", Integer.toBinaryString(rs2));
+
+							result = result + String.format("%5s", Integer.toBinaryString(rd));
+						}
+
+						//R2I Type
 					
-					if(optype1 == 0 && optype2 == 0 && optype3 == 0)		//R3 type Operation
+					}
+				}
+				else   							//RI type
+				{
+					if(opcode != 29 && opcode != 24)
 					{
 						int rs1 = sop1.getValue();
-						int rs2 = sop2.getValue();
 						int rd = dop.getValue();
+
 						result = result + String.format("%5s", Integer.toBinaryString(rs1));
-
-						result = result + String.format("%5s", Integer.toBinaryString(rs2));
-
 						result = result + String.format("%5s", Integer.toBinaryString(rd));
 					}
-					
 				}
 				res=Integer.parseInt(result); 
 				dout.writeInt(res);
