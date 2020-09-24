d32d33a1727d37ff36464468168bcd6674ed2f1c
cyberlust <rutikdadhaniya2034@gmail.com>
2020-09-24 15:18:32 +0530

    R3 Type Done. Please use same variables

--- Assignment 2/CS311_assignment2/src/generic/Simulator.java
+++ Assignment 2/CS311_assignment2/src/generic/Simulator.java
@@ -40,12 +40,54 @@ public class Simulator{
 			{
 				dout.writeInt(ParsedProgram.data.get(i));
 			}
+			String result = "";
+			int res=0;
+			for (int i = 0; i < ParsedProgram.code.size(); i++)
+			{
+				result = "";
+				int pc = ParsedProgram.code.get(i).getProgramCounter();
+				// dout.writeInt(pc);
+				Instruction.OperationType operation_type = ParsedProgram.code.get(i).operationType;
+				int opcode = operation_type.ordinal();
+				
+				result = result + String.format("%5s", Integer.toBinaryString(opcode));
+				
+				System.out.println(res);
+				
+
+				Operand sop1 = new Operand();
+				Operand sop2 = new Operand();
+				Operand dop = new Operand();
+
+				sop1= ParsedProgram.code.get(i).getSourceOperand1();
+				sop2= ParsedProgram.code.get(i).getSourceOperand2();
+				dop= ParsedProgram.code.get(i).getDestinationOperand();
+
+				if(opcode != 29 && opcode != 24)
+				{
+					int optype1 = sop1.getOperandType().ordinal();
+					int optype2 = sop2.getOperandType().ordinal();
+					int optype3 = dop.getOperandType().ordinal();	
+					
+					if(optype1 == 0 && optype2 == 0 && optype3 == 0)		//R3 type Operation
+					{
+						int rs1 = sop1.getValue();
+						int rs2 = sop2.getValue();
+						int rd = dop.getValue();
+						result = result + String.format("%5s", Integer.toBinaryString(rs1));
+
+						result = result + String.format("%5s", Integer.toBinaryString(rs2));
+
+						result = result + String.format("%5s", Integer.toBinaryString(rd));
+					}
+					
+				}
+				res=Integer.parseInt(result); 
+				dout.writeInt(res);
+			}
+			
 			bout.writeTo(fout);
-			// for (int i = 0; i < ParsedProgram.code.size(); i++)
-			// {
-			// 	fos.write(ParsedProgram.code.get(i).toString().getBytes("UTF-16"));
-			// }
-			 
+
 			fout.flush();
 			fout.close();
 		} 
