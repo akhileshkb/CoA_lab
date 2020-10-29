54b3b679701161ccb9a82526e2767e80516cc125
rajatyagi <50312914+rajatyagi@users.noreply.github.com>
2020-10-29 05:32:23 -0400

    Add files via upload

---
+++ Assignment 4/src/generic/Statistics.java
@@ -0,0 +1,42 @@
+package generic;
+
+import java.io.PrintWriter;
+
+public class Statistics {
+	
+	// TODO add your statistics here
+	static int numberOfInstructions;
+	static int numberOfCycles;
+	
+
+	public static void printStatistics(String statFile)
+	{
+		try
+		{
+			PrintWriter writer = new PrintWriter(statFile);
+			
+			writer.println("Number of instructions executed = " + numberOfInstructions);
+			writer.println("Number of cycles taken = " + numberOfCycles);
+			
+			//  TODO add code here to print statistics in the output file
+
+			// System.out.println("Number of instructions executed = " + numberOfInstructions);
+			// System.out.println("Number of cycles taken = " + numberOfCycles);
+			
+			writer.close();
+		}
+		catch(Exception e)
+		{
+			Misc.printErrorAndExit(e.getMessage());
+		}
+	}
+	
+	// TODO write functions to update statistics
+	public void setNumberOfInstructions(int numberOfInstructions) {
+		Statistics.numberOfInstructions = numberOfInstructions;
+	}
+
+	public void setNumberOfCycles(int numberOfCycles) {
+		Statistics.numberOfCycles = numberOfCycles;
+	}
+}
---
+++ Assignment 4/src/processor/pipeline/EX_MA_LatchType.java
@@ -0,0 +1,81 @@
+package processor.pipeline;
+
+public class EX_MA_LatchType {
+	
+	boolean MA_enable;
+	ControlUnit cu;
+	int ALUresult;
+	int rs2;
+	int rs1;
+	int rd;
+	
+	public EX_MA_LatchType()
+	{
+		MA_enable = false;
+	}
+
+	public boolean isMA_enable() {
+		return MA_enable;
+	}
+
+	public void setMA_enable(boolean mA_enable) {
+		MA_enable = mA_enable;
+	}
+
+	public int getALUResult()
+	{
+		return ALUresult;
+	}
+
+	public void setALUresult(int aluresult)
+	{
+		this.ALUresult = aluresult;
+	}
+
+	public void setRs2(int rs_2)
+	{
+		this.rs2 = rs_2;
+	}
+
+	public int getRs2()
+	{
+		return rs2;
+	}
+
+	public void setRs1(int rs_1)
+	{
+		this.rs1 = rs_1;
+	}
+
+	public int getRs1()
+	{
+		return rs1;
+	}
+
+	public void setRd(int rd)
+	{
+		this.rd = rd;
+	}
+
+	public int getRd()
+	{
+		return rd;
+	}
+
+	public void setControlUnit(ControlUnit cu) {
+		this.cu = cu;
+	}
+
+	public ControlUnit getControlUnit() {
+		return cu;
+	}
+
+	public void printState()
+	{  
+		System.out.println("rd = " + rd);
+		System.out.println("rs2 = " + rs2);
+		System.out.println("rs1 = " + rs1);
+		System.out.println("ALUresult = " + ALUresult);
+	}
+
+}
